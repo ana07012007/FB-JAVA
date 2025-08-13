b78101436d63774caa94c052626d00553b38cf4e
ana07012007 <ananya.7jan@gmail.com>
2025-08-13 09:17:57 +0530

    Add files via upload

---
+++ practise.java
@@ -0,0 +1,7 @@
+public class practise {
+    public static void main(String []args){
+        System.out.println("Name: Ananya");
+        System.out.println("class: FB");
+        System.out.println("roll no.: 6");
+    }
+}
